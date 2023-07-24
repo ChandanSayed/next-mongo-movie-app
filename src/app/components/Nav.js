054320959d66857c88ffdca4af6c9aab3e8001cc
unknown <hangzhou635@gmail.com>
2023-07-25 02:53:38 +0600

    Responsive Navbar done

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -1,26 +1,36 @@
+'use client';
+import React, { useState } from 'react';
 import Link from 'next/link';
+import { CgCloseR, CgMenu } from 'react-icons/cg';
 import styles from '../styles/navbar.module.css';
 
 const Nav = () => {
+  const [openMenu, setOpenMenu] = useState(false);
   const linkItems = [
-    { name: 'Home', targrt: '/' },
-    { name: 'About', targrt: '/about' },
-    { name: 'Movie', targrt: '/movies' },
-    { name: 'Contact', targrt: '/contact' },
+    { name: 'Home', target: '/' },
+    { name: 'About', target: '/about' },
+    { name: 'Movie', target: '/movies' },
+    { name: 'Contact', target: '/contact' }
   ];
   return (
-    <nav>
-      <ul className={styles.navbarList}>
-        {linkItems.map((item, i) => {
-          return (
-            <li className={styles.navbarItem} key={i}>
-              <Link className={styles.navbarLink} href={item.targrt}>
-                {item.name}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+    <nav className={styles.navbar}>
+      <div className={openMenu ? `${styles.active}` : ''}>
+        <ul className={styles.navbarList}>
+          {linkItems.map((item, i) => {
+            return (
+              <li className={styles.navbarItem} key={i}>
+                <Link className={styles.navbarLink} href={item.target} onClick={() => setOpenMenu(false)}>
+                  {item.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+        <div className={styles['mobile-navbar-btn']}>
+          <CgMenu name="menu-outline" className={styles['mobile-nav-icon']} onClick={() => setOpenMenu(true)} />
+          <CgCloseR name="close-outline" className={`${styles['mobile-nav-icon']} ${styles['close-outline']}`} onClick={() => setOpenMenu(false)} />
+        </div>
+      </div>
     </nav>
   );
 };
